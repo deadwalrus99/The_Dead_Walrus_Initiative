9a89a4ddbb34b08d7b91d4c271fd05fd0ddc252a
Dragos <dragos.budan@gmail.com>
2015-01-10 05:01:03 +0000

    Let's do this

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+volatile uint8_t * const HELL = (uint8_t *) 0x666;
+
+int main(int argc, char *argv[])
+{
+
+	//NIGGERS
+	while(1)
+	{
+		printf("The Ball is rolling: %d\n", &HELL); 		
+	}
+
+	return 0;
+}
