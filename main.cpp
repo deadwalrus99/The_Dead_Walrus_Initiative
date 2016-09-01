1a6f1a3bf48cebf327ab4b8184c0fabb32c27e86
TimDykes <timjdykes@gmail.com>
2016-09-01 16:53:20 +0100

    #67

--- main.cpp
+++ main.cpp
@@ -4,11 +4,9 @@ volatile uint8_t * const HELL = (uint8_t *) 0x666;
 
 int main(int argc, char *argv[])
 {
-
-	//NIGGERS
 	while(1)
 	{
-		printf("The Ball is rolling: %d\n", &HELL); 		
+		printf("The Ball stopped fairly quickly: %d\n", &HELL); 		
 	}
 
 	return 0;
